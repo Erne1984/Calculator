42e2438854f3b488998b9e1651a431a0f2888034
ernesto <ernestofamorim@gmail.com>
2024-02-07 13:57:37 -0300

    css

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+const btnOne = document.querySelector("#btn-1");
+const btnTwo = document.querySelector("#btn-2");
+const btnThree = document.querySelector("#btn-3");
+const btnFour = document.querySelector("#btn-4");
+const btnFive = document.querySelector("#btn-5");
+const btnSix = document.querySelector("#btn-6");
+const btnSeven = document.querySelector("#btn-7");
+const btnEight = document.querySelector("#btn-8");
+const btnNine = document.querySelector("#btn-9");
 
+const btnAdd = document.querySelector("#btn-add");
+const btnSubtract = document.querySelector("#btn-subtract");
 
 function add(a, b){
     return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
 }
